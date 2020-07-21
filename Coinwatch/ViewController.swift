c69925ea8bcf76a9d7a857bdcd035645972d2441
Amad Salmon <amad.salmon@icloud.com>
2020-07-21 13:32:09 +0200

    Initial Commit

---
+++ Coinwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coinwatch
+//
+//  Created by Amad Salmon on 21/07/2020.
+//  Copyright © 2020 Amad Salmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
