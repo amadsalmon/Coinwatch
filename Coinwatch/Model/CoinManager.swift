7dc00284a8e6a9a00bd583a969e87158f28074ec
Amad Salmon <amad.salmon@icloud.com>
2020-07-21 13:38:01 +0200

    initial commit

---
+++ Coinwatch/Model/CoinManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoinManager.swift
+//  Coinwatch
+//
+//  Created by Amad Salmon on 21/07/2020.
+//  Copyright © 2020 Amad Salmon. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
